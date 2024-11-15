ee6101d026d2d3cd5279fbaa24723624e9e6f394
maheen <maheenwaris.real@gmail.com>
2024-11-15 20:51:12 +0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// typewritter
+const textArray = [
+    "Pen Tester",
+    "Social Engineer",
+    "Python programmer"
+];
+
+let typeJsText = document.querySelector(".animatedText");
+let stringIndex = 0;
+let charIndex = 0;
+let isTyping = true;
+
+function typeJs() {
+    if (stringIndex < textArray.length) {
+        const currentString = textArray[stringIndex];
+
+        if (isTyping) {
+            if (charIndex < currentString.length) {
+                typeJsText.innerHTML += currentString.charAt(charIndex);
+                charIndex++;
+            } else {
+                isTyping = false;
+            }
+        } else {
+            if (charIndex > 0) {
+                typeJsText.innerHTML = currentString.substring(0, charIndex - 1);
+                charIndex--;
+            } else {
+                isTyping = true;
+                stringIndex++;
+
+                if (stringIndex >= textArray.length) {
+                    stringIndex = 0;
+                }
+
+                charIndex = 0;
+                typeJsText.innerHTML = "";
+            }
+        }
+    }
+}
+
+setInterval(typeJs, 100);
